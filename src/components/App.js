15449a980c88499eba8336c97e2a5cf8d92db96e
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-04 00:56:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,20 @@
-
 import React from "react";
 import './../styles/App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ItemDetail from './ItemDetail'
-import ItemList from './ItemList'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ItemDetail from './ItemDetail';
+import ItemList from './ItemList';
 
 const App = () => {
-  return (
-    <div>
-        <BrowserRouter>
-            <Routes>
-                {/* Home Route */}
-                <Route path="/" element={<ItemList />} />
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<ItemList />} />
+                    <Route path="/users/:id" element={<ItemDetail />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
-                {/* Dynamic User Route */}
-                <Route path="/users/:id" element={<ItemDetail />} />
-            </Routes>
-        </BrowserRouter>
-    </div>
-  )
-}
-
-export default App
+export default App;
